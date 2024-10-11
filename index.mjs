99a2caa739978f17b0d20b16490530fb5a18bd8a
Carlos Felipe Dalan Campanari <carloskampa@hotmail.com>
2024-10-11 16:12:02 -0300

    Buscando objetos do S3

--- index.mjs
+++ index.mjs
@@ -1,10 +1,22 @@
 import { log } from './log.mjs';
+import { s3 } from '@aws-sdk/client-s3';
+
+const s3client = new S3({ region: 'us-east-1' });
 
 export const handler = async event => {
-  log('Log de execução após GitHub Actions. Event: ' + JSON.stringify(event));
+  const record = event.Records[0];
+  const Bucket = record.s3.bucket.name;
+  const Key = record.s3.object.key;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(event),
-  };
+  const getObjectResult = await s3client.getObject({ Bucket, Key });
+
+  const mega_byte = 1024 * 1024;
+
+  if (getObjectResult.ContentLength > 1 * mega_byte) {
+    log('Objeto muito grande!');
+    return 'Objeto muito grande!';
+  }
+
+  log('Objeto de tamanho OK');
+  return 'Objeto de tamanho OK';
 };
